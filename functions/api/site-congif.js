a0d6dad2d4601d0a3cf8bd5c2168934a7e262696
tlottiejr <thomas.lottiejr@gmail.com>
2025-09-17 17:42:37 -0400

    Delete functions/api/site-congif.js

--- functions/api/site-congif.js
+++
@@ -1,9 +0,0 @@
-// Returns public site configuration (Turnstile site key).
-export async function onRequestGet({ env }) {
-  const data = {
-    turnstileSiteKey: env.TURNSTILE_SITE_KEY ? String(env.TURNSTILE_SITE_KEY) : ''
-  };
-  return new Response(JSON.stringify(data), {
-    headers: { 'content-type': 'application/json; charset=utf-8', 'cache-control': 'no-store' }
-  });
-}
