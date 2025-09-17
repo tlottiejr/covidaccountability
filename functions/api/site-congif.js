bc1144a34b958123aa57493de594df166a317c28
tlottiejr <thomas.lottiejr@gmail.com>
2025-09-17 17:02:29 -0400

    Create site-congif.js

---
+++ functions/api/site-congif.js
@@ -0,0 +1,11 @@
+// functions/api/site-config.js
+// Returns public site configuration (Turnstile site key).
+
+export async function onRequestGet({ env }) {
+  const data = {
+    turnstileSiteKey: env.TURNSTILE_SITE_KEY ? String(env.TURNSTILE_SITE_KEY) : ''
+  };
+  return new Response(JSON.stringify(data), {
+    headers: { 'content-type': 'application/json; charset=utf-8', 'cache-control': 'no-store' }
+  });
+}
