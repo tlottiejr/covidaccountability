73883246f96173c7bc5b50ad4175140c1ff171e6
tlottiejr <thomas.lottiejr@gmail.com>
2025-09-17 17:59:12 -0400

    Update site-config.js

--- functions/api/site-config.js
+++ functions/api/site-config.js
@@ -1,13 +1,7 @@
 // functions/api/site-config.js
-// Returns public site configuration (Turnstile site key).
 export async function onRequestGet({ env }) {
-  const data = {
-    turnstileSiteKey: env.TURNSTILE_SITE_KEY ? String(env.TURNSTILE_SITE_KEY) : ''
-  };
+  const data = { turnstileSiteKey: env.TURNSTILE_SITE_KEY ? String(env.TURNSTILE_SITE_KEY) : '' };
   return new Response(JSON.stringify(data), {
-    headers: {
-      'content-type': 'application/json; charset=utf-8',
-      'cache-control': 'no-store'
-    }
+    headers: { 'content-type': 'application/json; charset=utf-8', 'cache-control': 'no-store' }
   });
 }
